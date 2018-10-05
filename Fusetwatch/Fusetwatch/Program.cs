3e1dbd3efa249a4ab67961577b72e5d48704300b
Егор <blooddecember@gmail.com>
2018-10-05 13:34:53 +0300

    Создание

---
+++ Fusetwatch/Fusetwatch/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fusetwatch
+{
+    class Program
+    {
+        public string patch;
+
+        public static void Count(object obj)
+        {
+            Console.Clear();
+            Process[] runningProcessByName = Process.GetProcessesByName("Fuset");
+            if (runningProcessByName.Length != 0)
+            {
+                Console.WriteLine("Бот работает");
+            }
+            else
+            {
+                Console.WriteLine("Запуск бота...");
+                Process.Start(Convert.ToString(obj));
+                Thread.Sleep(10000);
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            string num = "";
+
+            if (File.Exists("Setting.txt"))
+            {
+                StreamReader sr = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + @"\Setting.txt");
+                //Read the first line of text
+                num = sr.ReadLine();
+                sr.Close();
+            }
+
+            TimerCallback tm = new TimerCallback(Count);
+            Timer timer = new Timer(tm, num, 0, 2000);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Fusetwatch/Fusetwatch/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Fusetwatch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Fusetwatch")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("227411c5-7fee-48a2-a44d-e1d12683a613")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
